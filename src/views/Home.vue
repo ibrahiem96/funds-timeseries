f58fdc5861e34d68a9960e17d9c16a1dd4fd0688
Ibrahiem <ibrahiem.mohammad@fitchratings.com>
2020-06-11 23:25:25 -0500

    v2/dev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <section class="hero">
     <div class="hero-body">
       <div class="container">
-        <h1>Funds Management for Masjid al Muminun</h1>
+        <h1 style="padding-top: 50px">Funds Management for Masjid al Muminun</h1>
         <h2>Providing support for a community masjid and transparency for our donors</h2>
         <h3>
           |
@@ -15,6 +15,12 @@
           </a>
           |
         </h3>
+        <h2 style="padding-top: 100px;">
+          Donate
+        </h2>
+        <a href="https://venmo.com/Zosharif100">
+            Donate through Venmo
+        </a>
       </div>
     </div>
   </section>
